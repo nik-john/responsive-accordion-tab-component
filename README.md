1be9c91418ea29dcc2c8a5ce199d2089d745ddd8
nik-john <nikhil.john.88@gmail.com>
2016-09-25 02:43:30 +0530

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# tabs-accordion
-Converting tabs to accordion depending on device width, based on pure css and html
+## Overview
+
+A simple example of a responsive HTML Tab component that transforms into an Accordion component upon resizing. 
+
+The current breakpoint is `768px`
+
+## Demos
+
+View [demo](https://nik-john.github.io/responsive-accordion-tab-component) of:
+
+#### [Pure CSS Solution](https://nik-john.github.io/responsive-accordion-tab-component/v1)
+#### [CSS + Javascript Solution](https://nik-john.github.io/responsive-accordion-tab-component/v2)
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-var root = 'http://jsonplaceholder.typicode.com';
-localStorage.removeItem('fetched');
+var root = 'https://jsonplaceholder.typicode.com';
+
+function clearLs() {
+	localStorage.removeItem('fetched');
+}
+clearLs();
 $(document).ready(function() {
 	$('#tab-5 + .tab-label').on('click', function() {
 		if (localStorage.getItem('fetched') !== 'true') {
@@ -10,9 +14,16 @@ $(document).ready(function() {
 				$('.dynamic-title').html(data.title);
 				$('.dynamic-text').html(data.body);
 				localStorage.setItem('fetched', 'true');
+				console.log("Success! Element populated with Dynamic Data!");
+
 			});
 		} else {
 			console.log("Aborting. Data has already been fetched.")
 		}
 	});
-})
+
+	$('.clear-ls').on('click', function() {
+		clearLs();
+		console.log("Session cleared!");
+	});
+});
